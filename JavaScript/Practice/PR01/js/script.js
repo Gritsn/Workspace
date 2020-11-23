c6e7f4c2fd1749d2f4fb70d3ff4cc0180648bcd5
RGitabs <67023167+RGitabs@users.noreply.github.com>
2020-11-23 17:37:47 +0200

    script.js created

--- JavaScript/Practice/PR01/js/script.js
+++ JavaScript/Practice/PR01/js/script.js
@@ -1,12 +1,21 @@
 //Choose a random color
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+// const button = document.querySelector('button')
+// const body = document.querySelector('body')
+// const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
-body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
+// body.style.backgroundColor = 'violet'
+// button.addEventListener('click', changeBackground)
 
-function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
+// function changeBackground(){
+// const colorIndex= parseInt(Math.random()*colors.length)
+// body.style.backgroundColor = colors[colorIndex]
+// }
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+const colors = ['green', 'blue', 'red', 'purple', 'yellow'];
+body.style.backgroundColor = 'green';
+button.addEventListener('click', changeBackgroundColor);
+function changeBackgroundColor() {
+  const colorIndex = parseInt(Math.random() * colors.length);
+  body.style.backgroundColor = colors[colorIndex];
 }
