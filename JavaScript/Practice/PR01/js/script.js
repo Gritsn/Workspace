77677af15713d382e0558b0a5093f462b44e74a2
RGitabs <67023167+RGitabs@users.noreply.github.com>
2020-11-23 17:46:43 +0200

    .code workspace updated

--- JavaScript/Practice/PR01/js/script.js
+++ JavaScript/Practice/PR01/js/script.js
@@ -1,15 +1,3 @@
-//Choose a random color
-// const button = document.querySelector('button')
-// const body = document.querySelector('body')
-// const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
-
-// body.style.backgroundColor = 'violet'
-// button.addEventListener('click', changeBackground)
-
-// function changeBackground(){
-// const colorIndex= parseInt(Math.random()*colors.length)
-// body.style.backgroundColor = colors[colorIndex]
-// }
 const button = document.querySelector('button');
 const body = document.querySelector('body');
 const colors = ['green', 'blue', 'red', 'purple', 'yellow'];
