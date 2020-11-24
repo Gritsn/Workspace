dfeba98d5e5151c2fc760d3d45f3e87851fa6086
RGitabs <67023167+RGitabs@users.noreply.github.com>
2020-11-24 08:52:01 +0200

    Completion of Project 2 - HEX Background Color Change

---
+++ Practice/JS/hex-color-background-changer-main/js/app.js
@@ -0,0 +1,23 @@
+// Assign document variables
+
+const body = document.querySelector('body');
+const button = document.querySelector('#btn');
+const hexVale = document.querySelector('#hex-value');
+
+//Creat an hex construct array
+const hexArray = [1, 2, 3, 4, 5, 6, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+//Add an event listener button click
+button.addEventListener('click', hexChange);
+
+//Hex change function
+function hexChange() {
+  let hex = '#';
+
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * hexArray.length);
+    hex += hexArray[index];
+  }
+  hexVale.textContent = hex;
+  body.style.backgroundColor = hex;
+}
